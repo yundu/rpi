406fe9ea30fbfdef51feef14c2d072fafb4a1655
Yun Du <Yun.Du@bazaarvoice.com>
2015-12-29 00:24:45 -0600

    script now saves latest IP to disk for reference

--- checkip.py
+++ checkip.py
@@ -8,23 +8,26 @@ parameters = csv.reader(open('checkip.csv','rb'), delimiter=',')
 parameters = next(parameters)
 
 if ip[0] != parameters[2]:
+	print "IP address has changed"
 	parameters[2] = ip[0]
 
-message = """From: From Yun
+	message = """From: From Yun
 To: To Yun 
 Subject: IP Check
+ip has changed to %s
+""" %(parameters[2])
 
-ip is currently %s
-""" % (parameters[2])
-
-try:
-	session = smtplib.SMTP('smtp.gmail.com', 587)
-	session.ehlo()
-	session.starttls()
-	session.ehlo()
-	session.login(parameters[0],parameters[1])
-	session.sendmail(parameters[0], [parameters[0]], message)
-	session.quit()
-	print "Successfully sent email"
-except SMTPException:
-   print "Error: unable to send email"
+	try:
+		session = smtplib.SMTP('smtp.gmail.com', 587)
+		session.ehlo()
+		session.starttls()
+		session.ehlo()
+		session.login(parameters[0],parameters[1])
+		session.sendmail(parameters[0], [parameters[0]], message)
+		session.quit()
+		
+		writer = csv.writer(open('checkip.csv', 'w'))
+		writer.writerows([parameters])
+		print "Successfully sent email"
+	except SMTPException:
+	   print "Error: unable to send email"
